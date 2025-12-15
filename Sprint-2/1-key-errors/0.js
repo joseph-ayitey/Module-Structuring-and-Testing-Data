fe41b65a4c59dafe5b81e15735d2d7396ebe3cf3
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2025-12-15 14:33:52 +0100

    I have explained and give my predictions before running codes

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,10 +1,15 @@
 // Predict and explain first...
 //  =============> write your prediction here
+  /*The function capitalize is trying to redeclare the variable 'str' inside its scope, which will cause a syntax error.
+  This is because 'str' is already declared as a parameter of the function.
+  To fix this, we should use a different variable name for the new string we are creating.
+  After fixing the error, the function should correctly capitalize the first letter of the input string*/
+ 
 
-// call the function capitalise with a string input
+// call the function capitalize with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
+function capitalize(str) {
   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
