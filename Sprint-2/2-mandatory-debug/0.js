9676820392b27d1cfaf2c5b06bec2f0f6282acec
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2025-12-15 18:39:04 +0100

    Fix multiply function to return value instead of just logging

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -17,5 +17,13 @@ console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
 
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);  // Now output: "The result of multiplying 10 and 32 is 320"
+// The function now returns the product, which is then correctly displayed in the console log
+
+
 
 
