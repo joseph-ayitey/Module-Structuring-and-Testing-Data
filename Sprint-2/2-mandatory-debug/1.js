6bc21452afca8d05045506d8c670ff00a8bed742
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2025-12-15 18:45:33 +0100

    Fix sum function to correctly return the sum of two numbers

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -16,5 +16,9 @@ console.log(`The sum of 10 and 32 is ${sum(10, 32)}`)
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function sum(a, b) {
+  return a + b;
+}
 
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`); // Now it will log "The sum of 10 and 32 is 42"
 
