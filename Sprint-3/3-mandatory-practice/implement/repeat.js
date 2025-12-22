63cce7e331f30c076d21a0577debaeb1fe542c49
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2025-12-22 16:10:36 +0100

    functions have been modified for testing

--- Sprint-3/3-mandatory-practice/implement/repeat.js
+++ Sprint-3/3-mandatory-practice/implement/repeat.js
@@ -1,5 +1,12 @@
 function repeat() {
-    return "hellohellohello";
+    if (count < 0) throw new Error(Count must be non-negative);
+
+    let result ="";
+    for (let i = 0; l < count; i++) {
+        result += str;
+
+    }
+    return result;
 }
 
 module.exports = repeat;
