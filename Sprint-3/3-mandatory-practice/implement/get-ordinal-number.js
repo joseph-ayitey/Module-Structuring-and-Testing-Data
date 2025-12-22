8b748508d98be77a2712a8cda9d69ae36e512218
OCC-L0125 <joseph.yaw.ayitey@gmail.com>
2025-12-22 15:59:04 +0100

    Functions been modified appropriately to run the tests

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,22 +1,16 @@
 function getOrdinalNumber(num) {
-    const j = num % 10;
-    const k = num % 100;
-    if (j === 1 && k !==11)   {
-        return num + "1st";
-    }   
-
-    if (j === 2 && k !== 12) {
-        return num + "2nd";
-    }
-
-    if (j === 3 && K !== 13) {
-        return num + "3rd";
-    }
-
-    if (j === 4 && K !== 14){
-        return num + "4th"
-    }
-
+	const j = num % 10;
+	const k = num % 100;
+	if (j === 1 && k !== 11) {
+		return num + "st";
+	}
+	if (j === 2 && k !== 12) {
+		return num + "nd";
+	}
+	if (j === 3 && k !== 13) {
+		return num + "rd";
+	}
+	return num + "th";
 }
 
 module.exports = getOrdinalNumber;
